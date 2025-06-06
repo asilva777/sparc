1b07c6c2b92e0707afdc36a4b740fd0ee20742bc
ASilva Innovations <106929764+asilva777@users.noreply.github.com>
2025-06-07 05:51:52 +0800

    db_config.php

---
+++ db_config.php
@@ -0,0 +1,14 @@
+<?php
+$host = 'localhost';
+$db   = 'sparc_db';
+$user = 'db_user';
+$pass = 'db_password';
+$charset = 'utf8mb4';
+
+$dsn = "mysql:host=$host;dbname=$db;charset=$charset";
+$options = [
+    PDO::ATTR_ERRMODE            => PDO::ERRMODE_EXCEPTION,
+    PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC,
+    PDO::ATTR_EMULATE_PREPARES   => false,
+];
+?>
